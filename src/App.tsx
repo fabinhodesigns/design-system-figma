f58d21c8e5f4cb0a35876811488583d3d688f8e2
fabinho070 <freitasfabiano08@gmail.com>
2023-01-05 10:42:45 -0300

    starting to build the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
-import './styles/global.css';
+
+import { Envelope, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput'; 
+import { Logo } from './Logo';
 
 export function App() {
   return (
-    <>
-      <h1 className="text-gray-100">Hello world</h1>
-      <button className="bg-purple-900 text-xl">Button</button>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Futurists Arts 
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e tenha conteúdos exclusivos!
+        </Text>
+      </header>
+
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-md mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text>Endereço de e-mail</Text>
+
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input type="email" id="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text>Sua Senha</Text>
+
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input type="password"  id="password" placeholder="******************" />
+          </TextInput.Root>
+        </label>
+      </form>
+    </div>
   )
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,9 +6,11 @@ export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
+
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
     const Comp = asChild ? Slot : 'span';
 
     return (
@@ -19,7 +21,8 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
                     'text-xs': size === 'sm',
                     'text-md': size === 'md',
                     'text-lg': size === 'lg',
-                }
+                },
+                className
             )}
         >
             {children}
